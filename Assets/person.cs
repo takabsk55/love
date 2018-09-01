64d43c88527f4860579212d430f69b5bebed7876
yamanishi <yamanishi@fine.cs.kobe-u.ac.jp>
2018-09-01 21:11:32 +0900

    見た目を作ってる

---
+++ Assets/person.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class person : MonoBehaviour {
+	public int myNumber;
+	public int yourNumber;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DrawLine(){
+		
+	}
+
+	public void OnClick(){
+		Clear ();
+		DrawLine ();
+		Debug.Log ("hoge");
+
+	}
+
+	void Clear(){
+		
+	}
+}
